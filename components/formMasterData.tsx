4e1e96cab2c13c03423e8541694e2504e2d0a32b
rizfanradya <rizfankusuma@gmail.com>
2024-08-07 13:17:10 +0000

    chore: Update npm dependencies for FormMasterData component

--- components/formMasterData.tsx
+++ components/formMasterData.tsx
@@ -1,22 +1,30 @@
 import { FontAwesome6 } from "@expo/vector-icons";
 import { useEffect, useRef, useState } from "react";
 import {
+  ActivityIndicator,
   Alert,
   Animated,
+  Button,
+  Dimensions,
   Image,
   StyleSheet,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as Location from "expo-location";
+import RNPickerSelect from "react-native-picker-select";
+const dimension = Dimensions.get("screen");
 
 export default function FormMasterData() {
   const [buttonAddData, setButtonAddData] = useState(false);
+  const [buttonLoading, setButtonLoading] = useState(false);
   const cameraAnim = useRef(new Animated.Value(0)).current;
   const formAnim = useRef(new Animated.Value(0)).current;
-  const [cameraUri, setCameraUri] = useState<string | null>(null);
+  const [cameraUri, setCameraUri] = useState<any>(null);
   const [description, setDescription] = useState<string>("");
+  const [choice_id, setChoice_id] = useState<number>(0);
 
   useEffect(() => {
     Animated.parallel([
@@ -57,15 +65,8 @@ export default function FormMasterData() {
   });
 
   const handleCamera = async () => {
+    setButtonLoading(false);
     const imagePerm = await ImagePicker.requestCameraPermissionsAsync();
-    const geoLocatePerm = await Location.requestForegroundPermissionsAsync();
-    if (geoLocatePerm.status !== "granted") {
-      Alert.alert(
-        "Izin diperlukan",
-        "Izin lokasi diperlukan untuk mengambil lokasi terkini."
-      );
-      return;
-    }
     if (imagePerm.status !== "granted") {
       Alert.alert(
         "Izin diperlukan",
@@ -82,36 +83,10 @@ export default function FormMasterData() {
     if (!resultImage.canceled) {
       setCameraUri(resultImage.assets[0].uri);
     }
-    const location = await Location.getCurrentPositionAsync({});
-    const data = {
-      datetime: new Date().toISOString(),
-      location,
-      image: resultImage.assets[0].uri,
-      description,
-      choice_id: 1,
-    };
-    alert(`
-      datetime    = ${data.datetime}
--------------------------------------------
-      location    = ${JSON.stringify(location)}
--------------------------------------------
-      image       = ${data.image}
--------------------------------------------
-      description = ${data.description}
--------------------------------------------
-      choice_id   = ${data.choice_id}
-      `);
   };
 
-  const handleForm = async () => {
-    const geoLocatePerm = await Location.requestForegroundPermissionsAsync();
-    if (geoLocatePerm.status !== "granted") {
-      Alert.alert(
-        "Izin diperlukan",
-        "Izin lokasi diperlukan untuk mengambil lokasi terkini."
-      );
-      return;
-    }
+  const handleFile = async () => {
+    setButtonLoading(false);
     const resultImage: any = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -121,38 +96,111 @@ export default function FormMasterData() {
     if (!resultImage.canceled) {
       setCameraUri(resultImage.assets[0].uri);
     }
-    const location = await Location.getCurrentPositionAsync({});
-    const data = {
-      datetime: new Date().toISOString(),
-      location,
-      image: resultImage.assets[0].uri,
-      description,
-      choice_id: 1,
-    };
-    alert(`
-      datetime    = ${data.datetime}
--------------------------------------------
-      location    = ${JSON.stringify(location)}
--------------------------------------------
-      image       = ${data.image}
--------------------------------------------
-      description = ${data.description}
--------------------------------------------
-      choice_id   = ${data.choice_id}
-      `);
   };
+
+  async function onSubmit() {
+    setButtonLoading(true);
+    const geoLocatePerm = await Location.requestForegroundPermissionsAsync();
+    if (geoLocatePerm.status !== "granted") {
+      Alert.alert(
+        "Izin diperlukan",
+        "Izin lokasi diperlukan untuk mengambil lokasi terkini."
+      );
+      return;
+    }
+    try {
+      const data = {
+        datetime: new Date().toISOString(),
+        location: await Location.getCurrentPositionAsync({}),
+        image: cameraUri,
+        description,
+        choice_id,
+      };
+      alert(`
+            datetime    = ${data.datetime}
+      -------------------------------------------
+            location    = ${JSON.stringify(data.location)}
+      -------------------------------------------
+            image       = ${data.image}
+      -------------------------------------------
+            description = ${data.description}
+      -------------------------------------------
+            choice_id   = ${data.choice_id}
+            `);
+    } catch (error: any) {
+      alert(error);
+    }
+    setButtonLoading(false);
+  }
 
   return (
     <>
       <>
         {cameraUri && (
-          <View style={styles.formContainer}>
-            <View style={styles.imageContainer}>
-              <Image
-                source={{ uri: cameraUri }}
-                style={styles.image}
-                resizeMode="cover"
-              />
+          <View style={styles.formMasterContainer}>
+            <View style={styles.formContainer}>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={{ uri: cameraUri }}
+                  style={styles.image}
+                  resizeMode="cover"
+                />
+              </View>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  multiline
+                  placeholder="Description..."
+                  numberOfLines={6}
+                  style={styles.textareaInputBase}
+                  onChangeText={setDescription}
+                />
+                <View style={styles.selectInputBase}>
+                  <RNPickerSelect
+                    onValueChange={setChoice_id}
+                    items={[
+                      {
+                        label: "Banjir",
+                        value: 1,
+                      },
+                      {
+                        label: "Pencemaran",
+                        value: 2,
+                      },
+                      {
+                        label: "Kebakaran",
+                        value: 3,
+                      },
+                    ]}
+                  />
+                </View>
+              </View>
+              <View style={styles.formButtonContainer}>
+                <View style={styles.formButton}>
+                  <Button
+                    color={"#F5365C"}
+                    title="CLOSE"
+                    onPress={() => setCameraUri(null)}
+                  />
+                </View>
+                <View style={styles.formButton}>
+                  {buttonLoading ? (
+                    <>
+                      <Button title="SAVE" disabled={true} />
+                      <ActivityIndicator
+                        size={"large"}
+                        style={styles.activityIndicator}
+                      />
+                    </>
+                  ) : (
+                    <Button
+                      color={"#2DCE89"}
+                      title="SAVE"
+                      disabled={!description || !choice_id}
+                      onPress={onSubmit}
+                    />
+                  )}
+                </View>
+              </View>
             </View>
           </View>
         )}
@@ -202,7 +250,7 @@ export default function FormMasterData() {
             },
           ]}
         >
-          <TouchableOpacity onPress={handleForm} style={styles.button}>
+          <TouchableOpacity onPress={handleFile} style={styles.button}>
             <View style={styles.buttonInner}>
               <FontAwesome6 name="image" style={styles.buttonIcon} />
             </View>
@@ -214,16 +262,21 @@ export default function FormMasterData() {
 }
 
 const styles = StyleSheet.create({
-  formContainer: {
-    backgroundColor: "white",
+  formMasterContainer: {
+    flex: 1,
     position: "absolute",
-    bottom: 20,
-    left: 20,
-    right: 20,
-    top: 50,
-    borderRadius: 20,
-    overflow: "hidden",
     zIndex: 9999,
+    width: dimension.width,
+    height: dimension.height,
+    padding: 20,
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  formContainer: {
+    backgroundColor: "#F4F5F7",
+    paddingBottom: 20,
+    borderRadius: 14,
+    overflow: "hidden",
   },
   imageContainer: {
     height: 250,
@@ -234,6 +287,60 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     height: 350,
     marginHorizontal: "auto",
+  },
+  formInputContainer: {
+    padding: 20,
+  },
+  selectInputBase: {
+    borderRadius: 8,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 2,
+      height: 4,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 8,
+    backgroundColor: "#fff",
+  },
+  textareaInputBase: {
+    borderRadius: 8,
+    marginBottom: 10,
+    textAlignVertical: "top",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    shadowColor: "#000",
+    backgroundColor: "#fff",
+    shadowOffset: {
+      width: 2,
+      height: 4,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 8,
+  },
+  inputLabel: {
+    marginBottom: 5,
+  },
+  formButtonContainer: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    gap: 10,
+    alignSelf: "flex-end",
+  },
+  formButton: {
+    width: 100,
+    borderRadius: 6,
+    overflow: "hidden",
+  },
+  activityIndicator: {
+    position: "absolute",
+    right: 0,
+    left: 0,
+    top: 0,
+    bottom: 0,
   },
   mainButton: {
     position: "absolute",
